9e16cc2fb04ad620a850f371252504b6bc17aa78
conorhalloran <conorhalloran@gmail.com>
2017-09-29 11:17:56 -0700

    Updated gitignore and Day5 notes

--- week3/day5/inner-join.rb
+++ week3/day5/inner-join.rb
@@ -30,3 +30,29 @@ INNER JOIN enrolments ON enrolments.student_id = students.id
 INNER JOIN courses ON enrolments.course_id = courses.id
 WHERE courses.title = 'Up-sized hybrid project';
 
+Select all students who do not have projects 
+
+SELECT *
+FROM students
+LEFT JOIN projects ON projects.student_id = students.id
+WHERE title IS NULL;
+
+--------------------------------
+
+SELECT students.*, COUNT(projects.created_at) as total FROM students
+INNER JOIN projects
+ON projects.student_id = students.id
+WHERE total = 0
+GROUP BY students.id
+
+List the average score for each course 
+SELECT course_id, ROUND(AVG(score)), courses.title
+FROM enrolments
+INNER JOIN courses ON courses.id = enrolments.course_id
+GROUP BY course_id, courses.title;
+
+SELECT courses.id, courses.title, ROUND( AVG(score) )
+FROM courses
+INNER JOIN enrolments ON courses.id = enrolments.course_id
+GROUP BY courses.id
+LIMIT 10;
