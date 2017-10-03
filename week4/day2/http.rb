c1e1017d900320032465eb90275c212f7c7d7226
conorhalloran <conorhalloran@gmail.com>
2017-10-03 10:14:41 -0700

    Add http

--- week4/day2/http.rb
+++ week4/day2/http.rb
@@ -4,3 +4,16 @@ OSI vs TCP/IP model
 
 IPv4 is a 32bit address. 
 4 * 8 bits or 4 bytes.
+
+traceroute www.reddit.com
+Routing Table:
+A routeer looks at incoming packets destination IPs and routes them according to a routing table. 
+
+IPv6 uses hexadecimal addresses
+2001:0DB8:AC10:FE01:: (when followed by all zeros use "::")
+
+localhost (loopback)
+IPv4: 127.0.0.1 or 0.0.0.0
+IPv6: 0:0:0:0:0:0:0:1 or ::1
+
+DNS: Domain Name System: translate a url into an ip address. Is provided by your ISP. 
